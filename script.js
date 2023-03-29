32773b156e60a2fb4ed0162f54ed00446208ec06
daraz24 <129292062+daraz24@users.noreply.github.com>
2023-03-29 22:20:44 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const card2 = document.getElementById("card2");
 const openButton = document.getElementById("open");
 const nextButton = document.getElementById("next");
 
+function flipCard(card, value) {
+  card.querySelector(".front").classList.add("card-flip");
+  card.querySelector(".back").classList.add("card-flip");
+  card.querySelector(".back").textContent = value;
+}
+
 openButton.addEventListener("click", () => {
   const items = [
     ...Array.from({ length: 26 }, (_, i) => String.fromCharCode(i + 65)),
@@ -12,13 +18,13 @@ openButton.addEventListener("click", () => {
   const alphabetOrEmoji = items[Math.floor(Math.random() * items.length)];
   const number = Math.floor(Math.random() * 10);
 
-  card1.textContent = alphabetOrEmoji;
-  card2.textContent = number;
+  flipCard(card1, alphabetOrEmoji);
+  flipCard(card2, number);
 });
 
 nextButton.addEventListener("click", () => {
-  card1.style.backgroundColor = "lightgray";
-  card1.textContent = "";
-  card2.style.backgroundColor = "lightgray";
-  card2.textContent = "";
+  card1.querySelector(".front").classList.remove("card-flip");
+  card1.querySelector(".back").classList.remove("card-flip");
+  card2.querySelector(".front").classList.remove("card-flip");
+  card2.querySelector(".back").classList.remove("card-flip");
 });
